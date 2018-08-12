3f5b759d11c523ec9e7513a555113ed809448818
Malgorzata Dominikowska <m.dominikowska@wp.pl>
2018-08-12 23:05:05 +0200

    configuration

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {HashRouter, Route, Link, Switch, NavLink} from 'react-router-dom';
+import Header from './Header.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Header>
+            </Header>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ({children}) => {
+    return (
+        <div>
+            <div>
+                logo
+            </div>
+            <div>
+                menu
+            </div>
+        </div>
+    )
+};
+
+export default Header;
